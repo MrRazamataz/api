1f6ba10425cf2312f1c25571da38685558adb654
MrRazamataz <mrrazamataz@mrrazamataz.ga>
2022-03-19 14:54:51 +0000

    add the files

---
+++ main.py
@@ -0,0 +1,40 @@
+# API testing at api.mrrazamataz.ga
+import urllib.parse, urllib.request, re
+from flask import Flask, request, abort, jsonify
+import json, sqlite3, requests
+
+app = Flask(__name__)
+
+
+@app.route('/yturl', methods=['GET'])
+def yturl():
+    if request.method == 'GET':
+        print(request.json)
+        search = request.args.get('search')
+        if search:
+            print(search)
+            query_string = urllib.parse.urlencode({"search_query": search})
+            formatUrl = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
+            search_results = re.findall(r"watch\?v=(\S{11})", formatUrl.read().decode())
+            clip = requests.get("https://www.youtube.com/watch?v=" + "{}".format(search_results[0]))
+            video_url = "https://www.youtube.com/watch?v=" + "{}".format(search_results[0])
+            result = {
+                "error": "False",
+                "video_url": video_url
+            }
+
+        else:
+            result = {
+                "error": "True",
+                "error_msg": "Missing param `search`. Usage: /yturl?search=song 2 blur"
+            }
+        return result
+    else:
+        abort(400)
+
+
+if __name__ == '__main__':
+    from waitress import serve
+
+    serve(app, host="0.0.0.0", port=9000)
+    # app.run(host='0.0.0.0', port=9000)
